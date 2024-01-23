3f797aa45abe3c87a67d4236ddcc4f9269f7f3fb
andru.mahardi <andru.mahardi@sehatq.com>
2024-01-23 17:33:54 +0700

    feat: created a prototype App Shell Pattern sample  on ./root

---
+++ root/app.js
@@ -0,0 +1,37 @@
+const externalUrl = "https://dev.to";
+
+(function () {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((_) => console.log("service worker registered"))
+        .catch((err) => console.log("service worker not registered", err));
+    });
+  }
+})();
+
+function fetchDocument() {
+  axios
+    .get(externalUrl)
+    .then((res) => {
+      const root = document.getElementById("root");
+      root.innerHTML = res.data;
+    })
+    .catch(console.log);
+
+  return false;
+}
+
+function attachIframe() {
+  const iframe = document.createElement("iframe");
+  iframe.setAttribute("src", externalUrl);
+  iframe.style.width = "100vw";
+  iframe.style.height = "100vh";
+
+  const root = document.getElementById("root");
+  root.innerHTML = "";
+  root.append(iframe);
+
+  return false;
+}
---
+++ root/readme.md
@@ -0,0 +1,17 @@
+# PWA x Shell Pattern
+
+This is a first prototype and a simple app on how App Shell Pattern works.
+
+To run this prototype execute following commands.
+
+```js
+// only execute this command if you are in the root directory
+// if you are already inside directory ./root then omit this command
+cd root
+
+npm install
+
+npm start
+```
+
+This prototype is using node v18.17.0 and npm v10.3.0
